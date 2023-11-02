d987e47e8561ac36ef68d55aab65411a3c268143
KofiFloki <jeremiah.martey@yahoo.com>
2023-11-02 16:51:57 +0000

    N queens challenge

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,113 +1,53 @@
 #!/usr/bin/python3
-"""N queens solution finder module.
+"""
+This module provides solution for an interview question
+`The N queens puzzle` in which there is a challenge of
+placing N non-attacking queens on an N x N chessboard
 """
 import sys
 
 
-solutions = []
-"""The list of possible solutions to the N queens problem.
-"""
-n = 0
-"""The size of the chessboard.
-"""
-pos = None
-"""The list of possible positions on the chessboard.
-"""
+def nqueens(n):
+    """ A function that provides the solution to the challenge """
+    if type(n) is not int:
+        # if the input is not a number
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        # if the input is lesser than 4
+        print("N must be at least 4")
+        sys.exit(1)
+
+    # A list to keep track of the good positions
+    result = []
+
+    def backtrack(queen, row_and_col, row_minus_col):
+        """ a helper function for the nqueens """
+        row = len(queen)
+        if row == n:
+            result.append(queen.copy())
+            return
+
+        for col in range(n):
+            if col not in queen and row + col not in row_and_col\
+               and row - col not in row_minus_col:
+                backtrack(queen + [col], row_and_col + [row + col],
+                          row_minus_col + [row - col])
+
+    backtrack([], [], [])
+
+    for queens in result:
+        print([[a, queens[a]]for a in range(n)])
 
 
-def get_input():
-    """Retrieves and validates this program's argument.
-
-    Returns:
-        int: The size of the chessboard.
-    """
-    global n
-    n = 0
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
         n = int(sys.argv[1])
-    except Exception:
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    return n
 
-
-def is_attacking(pos0, pos1):
-    """Checks if the positions of two queens are in an attacking mode.
-
-    Args:
-        pos0 (list or tuple): The first queen's position.
-        pos1 (list or tuple): The second queen's position.
-
-    Returns:
-        bool: True if the queens are in an attacking position else False.
-    """
-    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
-        return True
-    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
-
-
-def group_exists(group):
-    """Checks if a group exists in the list of solutions.
-
-    Args:
-        group (list of integers): A group of possible positions.
-
-    Returns:
-        bool: True if it exists, otherwise False.
-    """
-    global solutions
-    for stn in solutions:
-        i = 0
-        for stn_pos in stn:
-            for grp_pos in group:
-                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
-                    i += 1
-        if i == n:
-            return True
-    return False
-
-
-def build_solution(row, group):
-    """Builds a solution for the n queens problem.
-
-    Args:
-        row (int): The current row in the chessboard.
-        group (list of lists of integers): The group of valid positions.
-    """
-    global solutions
-    global n
-    if row == n:
-        tmp0 = group.copy()
-        if not group_exists(tmp0):
-            solutions.append(tmp0)
-    else:
-        for col in range(n):
-            a = (row * n) + col
-            matches = zip(list([pos[a]]) * len(group), group)
-            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
-            group.append(pos[a].copy())
-            if not any(used_positions):
-                build_solution(row + 1, group)
-            group.pop(len(group) - 1)
-
-
-def get_solutions():
-    """Gets the solutions for the given chessboard size.
-    """
-    global pos, n
-    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
-    a = 0
-    group = []
-    build_solution(a, group)
-
-
-n = get_input()
-get_solutions()
-for solution in solutions:
-    print(solution)
+    nqueens(n)
